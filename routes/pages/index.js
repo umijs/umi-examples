4bfb5cd35f3e0306ef1691637b0ba2580d21b4c7
chencheng <sorrycc@gmail.com>
2018-04-20 18:16:46 +0800

    add scroll-to-top routes

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -9,7 +9,9 @@ export default () =>
       <li><Link to="/users">/users</Link></li>
       <li><Link to="/users/posts/123/comments">/users/posts/123/comments</Link></li>
       <li><Link to="/404">/404</Link></li>
+      <li><Link to="/routes-dont-exists">/routes-dont-exists</Link></li>
       <li><Link to="/animated-transitions">/animated-transitions</Link></li>
+      <li><Link to="/scroll-to-top/a">/scroll-to-top/a</Link></li>
       <li><Link to="/exclude">/exclude (which will show the 404 page)</Link></li>
     </ul>
   </div>
--- routes/pages/list.js
+++ routes/pages/list.js
@@ -1,3 +1,9 @@
+import router from 'umi/router';
 
 export default () =>
-  <h1>/list</h1>
+  <>
+    <h1>/list</h1>
+    <button onClick={() => {
+      router.push('/list');
+    }}>test push to self</button>
+  </>
