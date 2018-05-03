9d0fd65c69f470a9e02c58d61900fcbefdb3d8ff
chencheng <sorrycc@gmail.com>
2018-05-03 11:34:32 +0800

    update with-dva

--- with-dva/src/layouts/index.js
+++ with-dva/src/layouts/index.js
@@ -1,4 +1,5 @@
 import withRouter from 'umi/withRouter';
+import dynamic from 'umi/dynamic';
 import { connect } from 'dva';
 import { IntlProvider } from 'react-intl';
 
@@ -8,15 +9,21 @@ function mapStateToProps(state) {
   };
 }
 
-export default withRouter(
-  connect(mapStateToProps)(props => {
-    return (
-      <IntlProvider locale="en">
-        <div>
-          <h1>MAIN LAYOUT {props.text}</h1>
-          {props.children}
-        </div>
-      </IntlProvider>
-    );
-  }),
-);
+export default dynamic(() => new Promise(resolve => {
+  console.log('delay start');
+  setTimeout(() => {
+    console.log('delay end');
+    resolve(withRouter(
+      connect(mapStateToProps)(props => {
+        return (
+          <IntlProvider locale="en">
+            <div>
+              <h1>MAIN LAYOUT {props.text}</h1>
+              {props.children}
+            </div>
+          </IntlProvider>
+        );
+      }),
+    ))
+  }, 2000);
+}));
