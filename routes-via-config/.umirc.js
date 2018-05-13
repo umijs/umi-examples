65badfee64fe3d1e5bbcb0b8c3a5d32acf495625
chencheng <sorrycc@gmail.com>
2018-05-13 10:12:31 +0800

    update routes-via-config

--- routes-via-config/.umirc.js
+++ routes-via-config/.umirc.js
@@ -1,3 +1,19 @@
 export default {
   exportStatic: false,
+  routes: [
+    {
+      path: '/',
+      exact: true,
+      component: 'a',
+    },
+    {
+      path: '/list',
+      component: 'b',
+    },
+    {
+      path: '/users/:name',
+      exact: true,
+      component: 'c',
+    },
+  ],
 };
