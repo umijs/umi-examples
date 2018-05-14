6e09a7b41e0eba3faef4ab52d8bb312c7a2971c6
xdf <xudafeng@126.com>
2018-05-14 11:56:10 +0800

    Feat/tweak lint (#6)
    * feat: import datahub for mock and proxy
    
    * doc: update readme

--- eleme-demo/.umirc.js
+++ eleme-demo/.umirc.js
@@ -1,6 +1,20 @@
+const path = require('path');
+
 export default {
   hd: true,
   exportStatic: {
     htmlSuffix: true,
   },
+  plugins: [
+    ['umi-plugin-datahub', {
+      proxy: {
+        '^/restapi': {
+          hub: 'eleme-demo',
+        },
+      },
+      store: path.join(__dirname, 'data'),
+    }],
+  ],
+  disableServiceWorker: true,
+  disableDynamicImport: !!process.env.COVERAGE,
 };
