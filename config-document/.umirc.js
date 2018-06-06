f70b8f6cbcc1962e0af823a3fca189b551ebd122
chencheng <sorrycc@gmail.com>
2018-06-06 11:40:00 +0800

    add config-document example

---
+++ config-document/.umirc.js
@@ -0,0 +1,10 @@
+
+export default {
+  pages: {
+    '/a': { document: './pages/a.ejs' },
+    '/b': { document: './pages/b.ejs' },
+  },
+  exportStatic: {
+    htmlSuffix: true,
+  },
+};
---
+++ config-document/pages/a.js
@@ -0,0 +1,2 @@
+
+export default () => <div>a</div>;
