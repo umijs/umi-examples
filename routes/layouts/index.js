5c29fb3ae912f35d5efaa934a3b40e381f6a0c3d
sorrycc <sorrycc@gmail.com>
2018-08-24 15:26:54 +0800

    update routes

--- routes/layouts/index.js
+++ routes/layouts/index.js
@@ -1,17 +1,26 @@
+import { Component } from 'react';
 import Breadcrumbs from './Breadcrumbs';
 
-export default ({ children }) => {
-  return (
-    <div>
-      <div>Header (layouts/index.js)</div>
-      <hr/>
-      <Breadcrumbs />
-      <hr/>
-      {
-        children
-      }
-      <hr/>
-      <div>Footer (layouts/index.js)</div>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    console.log('mount');
+  }
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <div>Header (layouts/index.js)</div>
+        <hr/>
+        <Breadcrumbs />
+        <hr/>
+        {
+          children
+        }
+        <hr/>
+        <div>Footer (layouts/index.js)</div>
+      </div>
+    );
+  }
 }
+
+export default App;
